331a6ad458dc0460efcfd8f4c3bb028071641616
Mannuck <61803826+Mannuck@users.noreply.github.com>
2021-10-25 11:19:34 -0500

    Primer cambio evento

--- js/Cambios.js
+++ js/Cambios.js
@@ -1,9 +1,4 @@
-$(".option").click(function(){
+$(".optain").click(function(){
     console.log('ded');
   });
 
-  $(document).ready(function(){
-
-    //instrucciones jquery
-    alert('dsdd');
-    });
